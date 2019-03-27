859f6c78e0653cc80a2fba21ca2653d990537320
MinecraftDawn <eric4902077@gmail.com>
2019-03-27 18:14:56 +0800

    Init

---
+++ Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Entity
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moving();
+        roateing();
+    }
+
+    public override void moving()
+    {
+        //往前
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.forward *speed);
+        }
+        //往後
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(Vector3.back*speed);
+        }
+        //往左
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(Vector3.left*speed);
+        }
+        //往右
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(Vector3.right*speed);
+        }
+    }
+
+    private void roateing()
+    {
+        //向左轉
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Rotate(0f,2f,0f);
+        }
+        //向左轉
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Rotate(0f,-2f,0f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Entity otherEntity = other.gameObject.GetComponent<Entity>();
+        if (otherEntity != null)
+        {
+            Damage(otherEntity.AttackPower);
+        }
+    }
+}
---
+++ Assets/Script/enemy/enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : Entity
+{
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        speed = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moving();
+    }
+
+    public override void moving()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        if (player != null)
+        {
+            this.gameObject.transform.LookAt(player.transform.position);
+            this.gameObject.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+    }
+}
